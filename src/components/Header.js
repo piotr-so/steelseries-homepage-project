0c88ce0aa873b6de0a840e017faa18b4741b931a
Piotr Solenczew <piotr.solenczew@gmail.com>
2019-04-08 22:20:38 +0200

    basic navigation layout

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Navigation from './Navigation';
+import styles from "./Header.module.scss";
+import logo from "../assets/logo.png";
+
+import basket from "../assets/Basket.svg";
+import userImg from "../assets/Male User.svg";
+
+
+const Header = () => (
+    <>
+    <header>
+        <div className={styles.appHeader}>
+            <div>
+                <img src={logo} alt="logo" />
+            </div>
+            
+            <Navigation />
+            
+
+            <div className={styles.userSection}>
+                <img src={basket} alt="basket" />
+                <img src={userImg} alt="user" />
+            </div>
+        </div>
+    </header>
+    <div className={styles.divAdd}><span>Hello world</span></div>
+    </>
+)
+
+
+export default Header;
---
+++ src/components/Navigation.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import styles from './Navigation.module.scss';
+import { CSSTransitionGroup } from 'react-transition-group';
+
+function FirstChild(props) {
+    const childrenArray = React.Children.toArray(props.children);
+    return childrenArray[0] || null;
+}
+
+class Dropdown extends Component {
+    state = {
+        displayMenu: false, //switching for development purpose
+    }
+
+    handleMouseEvent = () => {
+        this.setState({
+            displayMenu: !this.state.displayMenu,
+        })
+    }
+
+
+
+    render() {
+        return (
+            <nav>
+                <ul className={styles.menu}>
+                    <li className={styles.productsList} onMouseEnter={this.handleMouseEvent} onMouseLeave={this.handleMouseEvent}>
+                        <a href="#">PRODUCTS</a>
+                        <CSSTransitionGroup
+                            component={FirstChild}
+                            transitionName="reveal"
+                            transitionEnterTimeout={500}
+                            transitionLeaveTimeout={200}
+                        >
+                            {this.state.displayMenu ? (
+                                <div key={'dropdownMenu'} className={styles.dropMenu} >
+                                    <ul>
+                                        <li>Headset</li>
+                                        <li>Keyboard</li>
+                                        <li>Mouse</li>
+                                    </ul>
+                                </div>
+                            ) :
+                                (
+                                    null
+                                )
+                            }
+                        </CSSTransitionGroup>
+
+                    </li>
+                    <li>
+                        <a href="#">ESPORTS</a>
+
+                    </li>
+                    <li>
+                        <a href="#">SUPPORT</a>
+
+                    </li>
+                    <li>
+                        <a href="#">COMMUNITY</a>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Dropdown;
