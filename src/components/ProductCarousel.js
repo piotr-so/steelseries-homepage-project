2263d7dc6009cb3a2d9be628deca445e4b666d0b
Piotr Solenczew <piotr.solenczew@gmail.com>
2019-04-19 14:45:53 +0200

    ProductCarousel component refactoring

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -34,70 +34,111 @@ class ProductCarousel extends Component {
         animationComplete: true,
     }
 
-    handleLeftClick = () => {
-        const newFirstElem = this.state.carouselElements.slice(2, 3);
-        const newMidAndLastElem = this.state.carouselElements.slice(0, 2);
-        const sum = [...newFirstElem, ...newMidAndLastElem];
+    // first step function for handling click event, it's checking direction and rotating array elements
+    handleRotationInDirection = (direction) => {
+
+        this.setState({
+            switchAnimation: direction,
+        });
+
+        let newFirstElem = [];
+        let newMidAndLastElem = [];
+        let sum = [];
+
+        if (direction === 'left') {
+            newFirstElem = this.state.carouselElements.slice(2, 3);
+            newMidAndLastElem = this.state.carouselElements.slice(0, 2);
+            sum = [...newFirstElem, ...newMidAndLastElem];
+            this.arrayRotation(sum);
+        }
+        else {
+            newFirstElem = this.state.carouselElements.slice(1, 3);
+            newMidAndLastElem = this.state.carouselElements.slice(0, 1);
+            sum = [...newFirstElem, ...newMidAndLastElem];
+            this.arrayRotation(sum);
+        }
+    }
+
+    // second step function for handling click event, it's handling multiple click-events prevention, delaying next elements mount for the time of animation
+    // and conditionally rendering text for centered product element
+    arrayRotation = (rotatedElements) => {
 
         this.setState({
             renderElement: false,
-            switchAnimation: "left",
             animationComplete: !this.state.animationComplete,
-        })
-        this.timeoutOne = setTimeout(() => {
+        });
+
+        const switchArrayElems = () => {
             this.setState({
-                carouselElements: sum,
+                carouselElements: rotatedElements,
                 switchAnimation: 'none',
                 renderElement: true,
                 animationComplete: !this.state.animationComplete,
-            })
-        }, 500)
+            });
+
+            clearTimeout(newArrayMountTimeout);
+        }
+
+        let newArrayMountTimeout = setTimeout(switchArrayElems, 500);
     }
 
-    handleRightClick = () => {
-        const newFirstElem = this.state.carouselElements.slice(1, 3);
-        const newMidAndLastElem = this.state.carouselElements.slice(0, 1);
-        const sum = [...newFirstElem, ...newMidAndLastElem];
+    // function for changing class during animation
+    animationClassChanger = (element) => {
 
-        this.setState({
-            renderElement: false,
-            switchAnimation: "right",
-            animationComplete: !this.state.animationComplete,
-        })
-        this.timeoutOne = setTimeout(() => {
-            this.setState({
-                carouselElements: sum,
-                switchAnimation: 'none',
-                renderElement: true,
-                animationComplete: !this.state.animationComplete,
-            })
-        }, 500)
+        const { switchAnimation } = this.state;
+
+        const classStyles = {
+            middle: [styles.middleElement, styles.middleElementSwitchFromLeftMid, styles.middleElementSwitchFromRightMid],
+            left: [styles.leftElement, styles.leftElementSwitchFromLeftLeftElem, styles.leftElementSwitchFromRightLeftElem],
+            right: [styles.rightElement, styles.rightElementSwitchFromLeftRhtElem, styles.rightElementSwitchFromRightRhtElem],
+        };
+
+
+        if (element === 'middle') {
+            return giveClass(classStyles.middle)
+        }
+        else if (element === 'left') {
+            return giveClass(classStyles.left)
+        }
+        else if (element === 'right') {
+            return giveClass(classStyles.right)
+        }
+
+        function giveClass(whichAnimation) {
+            if (switchAnimation === 'none') {
+                return whichAnimation[0]
+            }
+            else if (switchAnimation === 'left') {
+                return whichAnimation[1]
+            }
+            else if (switchAnimation === 'right') {
+                return whichAnimation[2]
+            }
+        }
     }
 
-    componentWillUnmount() {
-        clearTimeout(this.timeoutOne);
+    // function for injecting background as in-line style or fixing mouse element background position
+    addBackgroundOrFixMouseElem = (number) => {
+        const { carouselElements } = this.state;
+
+        return carouselElements[number].name === "Mouse" ? 
+
+        { 'background': `url(${carouselElements[number].background}) no-repeat 0% 100%` } 
+        :
+        { 'background': `url(${carouselElements[number].background}) no-repeat` }
     }
 
     render() {
-        const { carouselElements, switchAnimation, renderElement, animationComplete } = this.state;
+        const { carouselElements, renderElement, animationComplete } = this.state;
         return (
             <div className={styles.wrapper}>
                 <h1>SELECT PRODUCT</h1>
                 <div
-                    className={
-                        switchAnimation === 'none' ? styles.middleElement
-                            :
-                            switchAnimation === 'left' ? styles.middleElementSwitchFromLeftMid
-                                :
-                                styles.middleElementSwitchFromRightMid
-                    }
+                    className={this.animationClassChanger('middle')}
                 >
                     <div
                         className={carouselElements[1].name === "Headset" ? styles.buttonFix : styles.item}
-                        style={
-                            carouselElements[1].name === "Mouse" ? { 'background': `url(${carouselElements[1].background}) no-repeat 0% 100%` } :
-                                { 'background': `url(${carouselElements[1].background}) no-repeat` }
-                        }
+                        style={this.addBackgroundOrFixMouseElem(1)}
                     >
                         <h3>{carouselElements[1].name}</h3>
                         {renderElement && (
@@ -112,36 +153,24 @@ class ProductCarousel extends Component {
                 </div>
                 <div className={styles.backElements}>
                     <div
-                        className={
-                            switchAnimation === 'none' ? styles.leftElement
-                                : switchAnimation === 'left' ? styles.leftElementSwitchFromLeftLeftElem :
-                                    styles.leftElementSwitchFromRightLeftElem}
-                        onClick={animationComplete ? this.handleLeftClick : undefined}
+                        className={this.animationClassChanger('left')}
+                        onClick={animationComplete ? () => this.handleRotationInDirection('left') : undefined}
                     >
                         <div
                             className={styles.item}
-                            style={
-                                carouselElements[0].name === "Mouse" ? { 'background': `url(${carouselElements[0].background}) no-repeat 0% 100%` } :
-                                    { 'background': `url(${carouselElements[0].background}) no-repeat` }
-                            }
+                            style={this.addBackgroundOrFixMouseElem(0)}
                         >
                             <h3>{carouselElements[0].name}</h3>
                             <img src={carouselElements[0].img} alt={carouselElements[0].name} />
                         </div>
                     </div>
                     <div
-                        className={
-                            switchAnimation === 'none' ? styles.rightElement
-                                : switchAnimation === 'left' ? styles.rightElementSwitchFromLeftRhtElem :
-                                    styles.rightElementSwitchFromRightRhtElem}
-                        onClick={animationComplete ? this.handleRightClick : undefined}
+                        className={this.animationClassChanger('right')}
+                        onClick={animationComplete ? () => this.handleRotationInDirection('right') : undefined}
                     >
                         <div
                             className={styles.item}
-                            style={
-                                carouselElements[2].name === "Mouse" ? { 'background': `url(${carouselElements[2].background}) no-repeat 0% 100%` } :
-                                    { 'background': `url(${carouselElements[2].background}) no-repeat` }
-                            }
+                            style={this.addBackgroundOrFixMouseElem(2)}
                         >
                             <h3>{carouselElements[2].name}</h3>
                             <img src={carouselElements[2].img} alt={carouselElements[2].name}></img>
