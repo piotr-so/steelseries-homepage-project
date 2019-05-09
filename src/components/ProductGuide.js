0714f31cd31568c9aa3b9f7f91b68fd0ae2cccb6
Piotr Solenczew <piotr.solenczew@gmail.com>
2019-05-09 22:57:20 +0200

    positioning fixes of previous components, needed for product-guide positioning; basic layout setting for product-guide

--- src/components/ProductGuide.js
+++ src/components/ProductGuide.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import styles from './ProductGuide.module.scss';
+import surveyData from '../assets/data/survey-data.json';
 
 class ProductGuide extends Component {
     render() {
         const { textToRender } = this.props;
-      return (
-        <div className={styles.wrapper}>
-        <h1>{textToRender}</h1>
-        </div>
-      );
+        return (
+            <div className={styles.wrapper}>
+                <h1>FIND THE PERFECT<br />GAMING {textToRender.toUpperCase()} FOR YOU</h1>
+                <div className={styles.cardWrapper}>
+                    <div className={`${styles.card} ${styles.card1}`}>
+                        <div className={styles.content}>
+                            <h2>QUESTION <span>#1</span></h2>
+                            <p>{surveyData['Mouse'].card1.question}</p>
+                            <div className={styles.variantsWrapper}>
+                                {surveyData['Mouse'].card1.answers.map(variants => (
+                                    <div>{variants}</div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`${styles.card} ${styles.card2}`}></div>
+                    <div className={`${styles.card} ${styles.card3}`}></div>
+                </div>
+
+            </div>
+        );
     }
-  }
-  
-  export default ProductGuide;
+}
+
+export default ProductGuide;
