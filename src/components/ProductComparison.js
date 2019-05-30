bd76ce930ed1daf57416bd9f2ed5b8b1176f8c3b
Piotr Solenczew <piotr.solenczew@gmail.com>
2019-05-30 21:20:23 +0200

    cleanup and positioning fixes

--- src/components/ProductComparison.js
+++ src/components/ProductComparison.js
@@ -2,131 +2,125 @@ import React, { Component } from 'react';
 import styles from "./ProductComparison.module.scss";
 import productsData from "../assets/data/products-comp-data.json";
 
+let wait = false;
+
+
 class ProductComparison extends Component {
     state = {
         isFixed: false,
-        wereScrolledBy: 0,
+        scrollTimesCounter: 0,
     }
 
-    tableHeadElementsFix = (e) => {
-        console.log('Works!');
+    tableHeadElementsFix = () => {
         this.setState(prevstate => ({
             isFixed: !prevstate.isFixed,
         }));
     }
 
+    clickTranslateX = (value) => {
+        const { scrollTimesCounter } = this.state;
+        const { quantityOfProducts, availableTimesToScroll } = this;
 
-    //dev
-    checktableposition = () => {
-        const body = document.body.getBoundingClientRect();
-        const tableElement = document.getElementsByClassName(styles.productsTable)[0];
-        const tableCoordinates = tableElement.getBoundingClientRect();
-        const tablePos = tableCoordinates.top;
-        console.log(tablePos);;
-        console.log(window.pageYOffset);
+        if (quantityOfProducts > 6) {
+            
+            if (value === 1 && scrollTimesCounter < availableTimesToScroll) {
+                this.setState(prevState =>( {
+                    scrollTimesCounter: prevState.scrollTimesCounter + value
+                }))
+            }
+            else if (value === -1 && scrollTimesCounter > 0) {
+                this.setState(prevState =>( {
+                    scrollTimesCounter: prevState.scrollTimesCounter + value
+                }))
+            }
+            
+        }   
+    }
+
+    throttle = (tableELemPosToWindow) => {
+        if (!wait && tableELemPosToWindow < 0 && this.state.isFixed !== true) {
+            window.requestAnimationFrame(() => {
+                wait = false;
+                this.tableHeadElementsFix()
+            });
+            wait = true;
+        }
+        else if (!wait && tableELemPosToWindow > 0 && this.state.isFixed !== false) {
+            window.requestAnimationFrame(() => {
+                wait = false;
+                this.tableHeadElementsFix()
+            });
+            wait = true;
+        }
     }
 
 
 
     componentDidMount() {
-        const tableWrapper = document.getElementsByClassName(styles.productsWrapper)[0];
         const tableElement = document.getElementsByClassName(styles.product)[0];
+        this.quantityOfProducts = productsData[this.props.productCategory].products.length;
+        this.availableTimesToScroll = this.quantityOfProducts - 6;
 
-        let wait = false;
-
-        const throttle = (tableELemPosToWindow) => {
-            if (!wait && tableELemPosToWindow < 0 && this.state.isFixed !== true) {
-                window.requestAnimationFrame(() => {
-                    wait = false;
-                    this.tableHeadElementsFix()
-                });
-                wait = true;
-            }
-            else if (!wait && tableELemPosToWindow > 0 && this.state.isFixed !== false) {
-                window.requestAnimationFrame(() => {
-                    wait = false;
-                    this.tableHeadElementsFix()
-                });
-                wait = true;
-            }
-        }
-
-        const scrollHandler = (scrolledByX) => {
-            if (!wait) {
-                window.requestAnimationFrame(() => {
-                    wait = false;
-                    this.setState({
-                        wereScrolledBy: scrolledByX
-                    })
-                    console.log(scrolledByX);
-                });
-                wait = true;
-            }
-        }
-
-        window.addEventListener('scroll', () => throttle(tableElement.getBoundingClientRect().top));
-        tableWrapper.addEventListener('scroll', () => scrollHandler(tableWrapper.scrollLeft));
+        window.addEventListener('scroll', () => this.throttle(tableElement.getBoundingClientRect().top));
     }
 
     render() {
         const { productCategory } = this.props;
         return (
             // Container width control depending on quantity of elements rendered (ensuring enough space is left for floating)
-            <div className={styles.table} style={{ 'width': `${130 + 160 * productsData[productCategory].products.length}px` }}>
-                <header>
+            <div className={styles.wrapper} style={{ 'width': `${130 + 160 * productsData[productCategory].products.length}px` }}>
+                <header className={styles.title}>
                     <h1>COMPARE OUR GAMING MOUSE LINEUP</h1>
                 </header>
                 <div className={styles.productsTable}>
-                    <div className={styles.features}>
-                        <div className={styles.featuresFillerBlock}></div>
-                        <div className={this.state.isFixed ? styles.featuresToFixed : undefined}></div>
-                        <ul>
-                            {/* CHANGE!!!!!!!!!!!!!! */}
-                            {productsData[productCategory].features.map(feature =>
-                                <li key={`id-${feature}`} onClick={() => this.checktableposition()}>{feature}</li>
+                {/*Sticky bar for scrolling table vertically */}
+                    <div className={`${styles.fixedFeaturesCover} ${this.state.isFixed ? styles.visible : styles.hidden}`}></div>
+                    <div className={`${styles.stickyBar} ${this.state.isFixed ? styles.visible : styles.hidden}`}>
+                        <div className={styles.stickyElementsWrapper} style={{'width': `${160 * productsData[productCategory].products.length}px`, 'transform': `translateX(-${this.state.scrollTimesCounter * 160}px)`}}>
+                            {productsData[productCategory].products.map((singleProduct, singleProductIdx) =>
+                                <div 
+                                    className={`
+                                    ${styles.stickyElement} 
+                                    ${singleProductIdx % 2 === 0 ? styles.headColorVar2 : singleProductIdx % 2 !== 0 && styles.headColorVar1}
+                                    `}
+                                    key={`sticky-item-${singleProductIdx}`}
+                                >
+                                    <h2>{singleProduct.name.toUpperCase()}</h2>
+                                    <div className={styles.stickyElementImg} style={{ 'background': `url(${singleProduct.url}) center no-repeat / cover` }} />
+                                </div>
                             )}
-                        </ul>
-                    </div>
-
-                    {this.state.isFixed &&
-                        <div className={styles.testHead}>
-                            <div className={styles.testHeadWrapper} style={{'width': `${50 + 160 * productsData[productCategory].products.length}px`}}>
-                                {productsData[productCategory].products.map((singleProduct, singleProductIdx) =>
-                                    <div className={styles.testHeadElement} style={this.state.isFixed ? {'transform': `translateX(-${this.state.wereScrolledBy}px)`} : undefined}>
-                                        <h2>{singleProduct.name.toUpperCase()}</h2>
-                                        <div className={styles.testHeadElementImg} style={{ 'background': `url(${singleProduct.url}) center no-repeat / cover` }} />
-                                    </div>
-                                )}
-                            </div>
                         </div>
-                    }
-
+                        <div className={`${styles.arrowLeft} ${this.state.scrollTimesCounter === 0 && styles.hidden}`} onClick={() => this.clickTranslateX(-1)}></div>
+                        <div className={`${styles.arrowRight} ${this.state.scrollTimesCounter === this.availableTimesToScroll && styles.hidden}`} onClick={() => this.clickTranslateX(1)}></div>    
+                    </div>
+                {/*  */}
+                {/* Features column */}
+                    <ul className={styles.productsFeatures}>
+                        {productsData[productCategory].features.map(feature =>
+                            <li key={`id-${feature}`}>{feature}</li>
+                        )}
+                    </ul>
+                {/*  */}
+                {/* Products */}
                     <div className={styles.productsWrapper}>
-
-                        <ul className={styles.columns} style={{ 'width': `${50 + 160 * productsData[productCategory].products.length}px` }}>
+                    
+                        <ul className={styles.columns} style={{ 'width': `${160 * productsData[productCategory].products.length}px`, 'transform': `translate(-${this.state.scrollTimesCounter * 160}px)` }}>
 
                             {productsData[productCategory].products.map((singleProduct, singleProductIdx) =>
                                 <li className={styles.product} key={`product-column-${singleProduct.name}`}>
                                     <div className={styles.tableHeadFiller}></div>
                                     {/* // Head of the product // */}
-                                    <div 
-                                        className={`
-                                            ${styles.head} 
-                                            ${this.state.isFixed ? styles.headFixed : undefined} 
-                                            ${this.state.isFixed && singleProductIdx % 2 === 0 ? styles.headColorVar1 : this.state.isFixed && singleProductIdx % 2 !== 0 ? styles.headColorVar2 : undefined}
-                                            `}
-                                        style={this.state.isFixed ? {'transform': `translateX(-${this.state.wereScrolledBy}px)`} : undefined}
-                                        // style={{'color': 'red'}}
-                                        // onScroll={() => this.scrollHandler()}
-                                        >
+                                    <div className={styles.head}>
                                         <h2>{singleProduct.name.toUpperCase()}</h2>
                                         <div className={styles.productImg} style={{'background': `url(${singleProduct.url}) center no-repeat / cover`}}/> 
                                     </div>
+                                    {/*  */}
                                     {/* // Single product features // */}
-                                    <ul className={`${styles.featuresList} ${singleProductIdx % 2 === 0 ? styles.colorVar2 : styles.colorVar1}`}>
+                                    <ul className={`${styles.featuresList} ${singleProductIdx % 2 === 0 ? styles.colorVar1 : styles.colorVar2}`}>
                                         {Object.entries(singleProduct).map((feature, featureIdx) => feature[0] === "name" || feature[0] === "url" ? undefined :
                                             <li key={singleProduct.name + '_' + (featureIdx + 1)}
-                                                className={feature[1] === "yes" ? styles.tickIcon : undefined}    >
+                                                className={feature[1] === "yes" ? styles.tickIcon : undefined}
+                                            >
                                                 {feature[1] === "no" || feature[1] === "yes" ? "" : feature[1]}
                                             </li>
                                         )}
@@ -134,9 +128,9 @@ class ProductComparison extends Component {
                                     {/* // */}
                                 </li>
                             )}
-
                         </ul>
-
+                        <div className={`${styles.arrowLeft} ${this.state.scrollTimesCounter === 0 && styles.hidden}`} onClick={() => this.clickTranslateX(-1)}></div>
+                        <div className={`${styles.arrowRight} ${this.state.scrollTimesCounter === this.availableTimesToScroll && styles.hidden}`} onClick={() => this.clickTranslateX(1)}></div>
                     </div>
                 </div>
             </div>
