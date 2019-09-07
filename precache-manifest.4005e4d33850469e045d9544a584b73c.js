1816a19a22ef48a6b2cd0f26c81168b461c8ad03
Piotr Solenczew <piotr.solenczew@gmail.com>
2019-09-07 11:27:50 +0200

    Updates

---
+++ precache-manifest.4005e4d33850469e045d9544a584b73c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a9f35b609e0076b38744e096f5c32f",
+    "url": "/steelseries-homepage-project/index.html"
+  },
+  {
+    "revision": "6da65fc5c62adf08b3c0",
+    "url": "/steelseries-homepage-project/static/css/main.be4489fb.chunk.css"
+  },
+  {
+    "revision": "17f205830b9715113cbf",
+    "url": "/steelseries-homepage-project/static/js/2.f30fe839.chunk.js"
+  },
+  {
+    "revision": "6da65fc5c62adf08b3c0",
+    "url": "/steelseries-homepage-project/static/js/main.f0e58f32.chunk.js"
+  },
+  {
+    "revision": "03bb0b35c8d09276f19b",
+    "url": "/steelseries-homepage-project/static/js/runtime~main.21b00207.js"
+  },
+  {
+    "revision": "f6af1e1d3c09dc6bdf1fd56d47d88d57",
+    "url": "/steelseries-homepage-project/static/media/Background-b.f6af1e1d.png"
+  },
+  {
+    "revision": "4da15e7ccc58597782ed386eda6f2a21",
+    "url": "/steelseries-homepage-project/static/media/Background.4da15e7c.png"
+  },
+  {
+    "revision": "fd8473b9cffd9950bb517f80ea1215db",
+    "url": "/steelseries-homepage-project/static/media/Basket.fd8473b9.svg"
+  },
+  {
+    "revision": "f194054d55965cfc6a64653d7412d776",
+    "url": "/steelseries-homepage-project/static/media/Exclamation_encircled.f194054d.svg"
+  },
+  {
+    "revision": "94445a0c856c3b70b2385ae2791f702e",
+    "url": "/steelseries-homepage-project/static/media/Headset_mod.94445a0c.png"
+  },
+  {
+    "revision": "2327e313f3b845418c9c3bc246c34191",
+    "url": "/steelseries-homepage-project/static/media/Layer 12.2327e313.png"
+  },
+  {
+    "revision": "e51830b1bf3f502f5d20e7b320a6f58b",
+    "url": "/steelseries-homepage-project/static/media/Layer10.e51830b1.png"
+  },
+  {
+    "revision": "33027a358a0e139fb9244530c5fbf16d",
+    "url": "/steelseries-homepage-project/static/media/Layer7.33027a35.png"
+  },
+  {
+    "revision": "ed837d2b00e7f43a48fab89ed596cd77",
+    "url": "/steelseries-homepage-project/static/media/Male User.ed837d2b.svg"
+  },
+  {
+    "revision": "b342d8867dc93236e15d1cf8cc704788",
+    "url": "/steelseries-homepage-project/static/media/SelectedTick.b342d886.svg"
+  },
+  {
+    "revision": "4ce529281fdad9ec21acf7ad2cb200bb",
+    "url": "/steelseries-homepage-project/static/media/apex300_leadin_proof2_r.4ce52928.png"
+  },
+  {
+    "revision": "4e78666fbb9dec4c55dac27d5ac3b4bf",
+    "url": "/steelseries-homepage-project/static/media/arctis2.4e78666f.png"
+  },
+  {
+    "revision": "802b8e5c5642fe12232e67b1955436bb",
+    "url": "/steelseries-homepage-project/static/media/arrow.802b8e5c.svg"
+  },
+  {
+    "revision": "1895b1dc9deebb5c4ee0da234706c49e",
+    "url": "/steelseries-homepage-project/static/media/arrowsDownSymbol.1895b1dc.svg"
+  },
+  {
+    "revision": "ff72f628511b79417a5afe64753689dd",
+    "url": "/steelseries-homepage-project/static/media/background_productGuide.ff72f628.png"
+  },
+  {
+    "revision": "fbcf968cc064e86e84e3a8a657feea51",
+    "url": "/steelseries-homepage-project/static/media/headset_background.fbcf968c.png"
+  },
+  {
+    "revision": "2f02a21a34054ddba3ac1abda0e0a41d",
+    "url": "/steelseries-homepage-project/static/media/left-arrow.2f02a21a.svg"
+  },
+  {
+    "revision": "78c400d0bff93f93f60258e6b65d25ad",
+    "url": "/steelseries-homepage-project/static/media/rival_100 copy.78c400d0.png"
+  },
+  {
+    "revision": "d3bd2f59407ea7cca625f48b0c3ab614",
+    "url": "/steelseries-homepage-project/static/media/slider-arrow.d3bd2f59.svg"
+  },
+  {
+    "revision": "30aba8416ced2763e1b98806115fd6cc",
+    "url": "/steelseries-homepage-project/static/media/steelseries-mice_mod.30aba841.png"
+  },
+  {
+    "revision": "74f64d82397194038f906874271a03be",
+    "url": "/steelseries-homepage-project/static/media/tick.74f64d82.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/steelseries-homepage-project/precache-manifest.236ef75b9598ba6ec916c01867e20726.js"
+  "/steelseries-homepage-project/precache-manifest.4005e4d33850469e045d9544a584b73c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/steelseries-homepage-project/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/steelseries-homepage-project/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
